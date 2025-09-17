ed9c5f7a38515f4e145057b7e140d93836ad7e41
willianmmarinho <willianmmarinho@gmail.com>
2025-09-17 17:24:27 -0300

    Subindo de pasta para facilitar o acesso ao index.html

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>Essenciais do React</h1>
+        <p>
+          Conceitos fundamentais do React que você vai precisar para quase
+          qualquer aplicação que for construir!
+        </p>
+      </header>
+      <main>
+        <h2>Hora de começar!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
