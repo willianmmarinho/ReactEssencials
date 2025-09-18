b05efd5e3ec1f3182f4d01ac0bef846b4adf667e
willianmmarinho <willianmmarinho@gmail.com>
2025-09-18 20:19:38 -0300

    feat(all) - finalização do capítulo 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import { CORE_CONCEPTS } from './data';
-import Header from './components/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Por favor selecione um tópico.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Header />
@@ -10,13 +37,21 @@ function App() {
         <section id="core-concepts">
           <h2>Conceitos centrais</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
           </ul>
         </section>
+        <br />
         <h2>Hora de começar!</h2>
+        <section id="examples">
+          <h2>Exemplos Práticos</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Componentes</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -29,3 +29,54 @@ export const CORE_CONCEPTS = [
       'Dados gerenciados pelo React que, quando alterados, fazem o componente ser renderizado novamente e a interface do usuário ser atualizada.',
   },
 ];
+
+export const EXAMPLES = {
+  components: {
+    title: 'Componentes',
+    description:
+      'Componentes são os blocos de construção das aplicações React. Um componente é um módulo independente (HTML + CSS opcional + JS) que renderiza uma saída.',
+    code: `
+    function Welcome() {
+      return <h1>Hello, World!</h1>;
+    }`,
+  },
+  jsx: {
+    title: 'JSX',
+    description:
+      'JSX é uma extensão de sintaxe do JavaScript. É semelhante a uma linguagem de template, mas possui todo o poder do JavaScript (por exemplo, pode gerar conteúdo dinâmico).',
+    code: `
+    <div>
+      <h1>Welcome {userName}</h1>
+      <p>Time to learn React!</p>
+    </div>`,
+  },
+  props: {
+    title: 'Props',
+    description:
+      'Os componentes aceitam entradas arbitrárias chamadas props. Elas são como os argumentos de uma função.',
+    code: `
+    function Welcome(props) {
+      return <h1>Hello, {props.name}</h1>;
+    }`,
+  },
+  state: {
+    title: 'State',
+    description:
+      'O state permite que os componentes do React mudem sua saída ao longo do tempo em resposta a ações do usuário, respostas da rede e qualquer outro evento.',
+    code: `
+    function Counter() {
+      const [isVisible, setIsVisible] = useState(false);
+
+      function handleClick() {
+        setIsVisible(true);
+      }
+
+      return (
+        <div>
+          <button onClick={handleClick}>Show Details</button>
+          {isVisible && <p>Amazing details!</p>}
+        </div>
+      );
+    }`,
+  },
+};
